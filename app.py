cb71196675d43ce2669e4db692ba69fcdacdcdbc
Derick Olson <olsonderick@gmail.com>
2023-02-28 06:34:16 -0500

    fix cors

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, request, abort
+from flask_cors import CORS
 from os import getenv 
 import requests
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/")
 def hello_world():
         return "<p>Hello world</p>"
 
 
-@app.route("/books")
+@app.route("/api/books")
 def get_books():
-        payload = {"list":"hardcover-fiction", "api-key": getenv("NYT_API_KEY") }
-        print(getenv("NYT_API_KEY")) 
-        r = requests.get("https://api.nytimes.com/svc/books/v3/lists.json", params=payload)
-        print(r.url)
-        return r.text
+        if request.args.get("token") == getenv("API_SECRET"):
+                payload = {"list":"hardcover-fiction", "api-key": getenv("NYT_API_KEY") }
+                r = requests.get("https://api.nytimes.com/svc/books/v3/lists.json", params=payload)
+                return r.text
+        else:
+                abort(401)
 
 
